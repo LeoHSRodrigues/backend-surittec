2e78f582914dad31869c004d839e23b3cbc1895a
leo23fla <leo23fla@gmail.com>
2019-12-05 15:12:11 -0300

    Inicio do projeto

---
+++ src/main/java/com/surittec/backendSurittec/BackendSurittecApplication.java
@@ -0,0 +1,13 @@
+package com.surittec.backendSurittec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSurittecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSurittecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surittec/backendSurittec/BackendSurittecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surittec.backendSurittec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSurittecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
